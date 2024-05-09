932adbc64984cc1515f4794c1ec968c7b1a6b71c
cuonglm <cuong.dz.1806@gmail.com>
2024-05-09 17:10:08 +0700

    json server config

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,6 @@
 export const keyLocalStorageListSP = "DANHSACHSP";
 export const keyLocalStorageItemCart = "DANHSACHITEMCART";
 
-export const locationAPIRoot = "https://provinces.open-api.vn/api";
+// export const locationAPIRoot = "https://provinces.open-api.vn/api";
+export const locationAPIRoot = "https://vapi.vnappmob.com/api";
 export const orderAPIRoot = "http://localhost:3000";
