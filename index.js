08d026ae4f7c8d61c0449558fbefdb626ff8e727
cuonglm <cuong.dz.1806@gmail.com>
2024-05-09 17:02:10 +0700

    json server config

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ import { getLocalStorage, setLocalStorage } from "./js/localStorage.js";
 const data = getLocalStorage(keyLocalStorageListSP);
 
 data.length !== 0 ? data : setLocalStorage(keyLocalStorageListSP, listData);
+
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
